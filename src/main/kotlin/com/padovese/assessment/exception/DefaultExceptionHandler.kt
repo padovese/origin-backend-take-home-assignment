685c89e9733d136df6edab8213d3274f0ccf2a7c
padovese <bpadovese@uolinc.com>
2020-05-24 14:31:53 -0300

    feat: handling wrong JSONs

--- src/main/kotlin/com/padovese/assessment/exception/DefaultExceptionHandler.kt
+++ src/main/kotlin/com/padovese/assessment/exception/DefaultExceptionHandler.kt
@@ -4,6 +4,7 @@ import org.slf4j.Logger
 import org.slf4j.event.Level
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
+import org.springframework.http.converter.HttpMessageNotReadableException
 import org.springframework.web.bind.annotation.ControllerAdvice
 import org.springframework.web.bind.annotation.ExceptionHandler
 
@@ -14,6 +15,12 @@ class DefaultExceptionHandler(private val logger: Logger) {
     fun handleError(exception: Exception): ResponseEntity<ErrorResponse> {
         logException(exception.message.toString(), exception, Level.ERROR)
         return getDefaultResponseMessage(exception.message.toString(), HttpStatus.INTERNAL_SERVER_ERROR)
+    }
+
+    @ExceptionHandler(HttpMessageNotReadableException::class)
+    fun handleJsonParseError(exception: HttpMessageNotReadableException): ResponseEntity<ErrorResponse> {
+        logException(exception.message.toString(), exception, Level.WARN)
+        return getDefaultResponseMessage("Invalid JSON.", HttpStatus.BAD_REQUEST)
     }
 
     @ExceptionHandler(NegativeArgException::class)
